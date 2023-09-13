9b6d9b144f908d577f9f44ef3ffe97310d5dc6b5
Saksham Thapa <56692835+Saksham010@users.noreply.github.com>
2023-09-13 17:37:48 +0545

    Update DownloadButton.js

--- landing-app/src/DownloadButton.js
+++ landing-app/src/DownloadButton.js
@@ -4,7 +4,7 @@ const DownloadButton = () => {
   const downloadFile = () => {
     // Change the filename to match your dist folder or archive file
     const fileName = 'dist.zip';
-    const filePath = `/dist/${fileName}`;
+    const filePath = `./${fileName}`;
     const link = document.createElement('a');
     link.href = filePath;
     link.download = fileName;
